824c68927cc6fc4903355c7e567c2ec8a5566913
Carol-seven <542046605@qq.com>
2022-07-14 13:56:41 +0800

    data_china

---
+++ R/data_china.R
@@ -0,0 +1,139 @@
+#' @name china_2002_122
+#' @docType data
+#' @title Input-output table for China, 2002, 122 sectors
+#' @description The national input-output table of China for 2002, covering 122 sectors.
+#'     Data are calculated at producers' prices in 2002.
+#'     Values are denoted in 10 thousand Chinese Yuan (CNY).
+#' @usage data(china_2002_122)
+#' @format A data frame with 129 rows and 138 columns.
+#' @source \href{https://data.stats.gov.cn/ifnormal.htm?u=/files/html/quickSearch/trcc/trcc01.html&h=740}{National Bureau of Statistics of China}
+"china_2002_122"
+
+
+#' @name china_2005_42
+#' @docType data
+#' @title Input-output table for China, 2005, 42 sectors
+#' @description The national input-output table of China for 2005, covering 42 sectors.
+#'     Data are calculated at producers' prices in 2005.
+#'     Values are denoted in 10 thousand Chinese Yuan (CNY).
+#' @usage data(china_2005_42)
+#' @format A data frame with 49 rows and 54 columns.
+#' @source \href{https://data.stats.gov.cn/ifnormal.htm?u=/files/html/quickSearch/trcc/trcc01.html&h=740}{National Bureau of Statistics of China}
+"china_2005_42"
+
+
+#' @name china_2007_135
+#' @docType data
+#' @title Input-output table for China, 2007, 135 sectors
+#' @description The national input-output table of China for 2007, covering 135 sectors.
+#'     Data are calculated at producers' prices in 2007.
+#'     Values are denoted in 10 thousand Chinese Yuan (CNY).
+#' @usage data(china_2007_135)
+#' @format A data frame with 142 rows and 151 columns.
+#' @source \href{https://data.stats.gov.cn/ifnormal.htm?u=/files/html/quickSearch/trcc/trcc01.html&h=740}{National Bureau of Statistics of China}
+"china_2007_135"
+
+
+#' @name china_2010_41
+#' @docType data
+#' @title Input-output table for China, 2010, 41 sectors
+#' @description The national input-output table of China for 2010, covering 41 sectors.
+#'     Data are calculated at producers' prices in 2010.
+#'     Values are denoted in 10 thousand Chinese Yuan (CNY).
+#' @usage data(china_2010_41)
+#' @format A data frame with 48 rows and 57 columns.
+#' @source \href{https://data.stats.gov.cn/ifnormal.htm?u=/files/html/quickSearch/trcc/trcc01.html&h=740}{National Bureau of Statistics of China}
+"china_2010_41"
+
+
+#' @name china_2012_139
+#' @docType data
+#' @title Input-output table for China, 2012, 139 sectors
+#' @description The national input-output table of China for 2012, covering 139 sectors.
+#'     Data are calculated at producers' prices in 2012.
+#'     Values are denoted in 10 thousand Chinese Yuan (CNY).
+#' @usage data(china_2012_139)
+#' @format A data frame with 146 rows and 154 columns.
+#' @source \href{https://data.stats.gov.cn/ifnormal.htm?u=/files/html/quickSearch/trcc/trcc01.html&h=740}{National Bureau of Statistics of China}
+"china_2012_139"
+
+
+#' @name china_2015_42
+#' @docType data
+#' @title Input-output table for China, 2015, 42 sectors
+#' @description The national input-output table of China for 2015, covering 42 sectors.
+#'     Data are calculated at producers' prices in 2015.
+#'     Values are denoted in 10 thousand Chinese Yuan (CNY).
+#' @usage data(china_2015_42)
+#' @format A data frame with 49 rows and 58 columns.
+#' @source \href{https://data.stats.gov.cn/ifnormal.htm?u=/files/html/quickSearch/trcc/trcc01.html&h=740}{National Bureau of Statistics of China}
+"china_2015_42"
+
+
+#' @name china_2017_149
+#' @docType data
+#' @title Input-output table for China, 2017, 149 sectors
+#' @description The national input-output table of China for 2017, covering 149 sectors.
+#'     Data are calculated at producers' prices in 2017.
+#'     Values are denoted in 10 thousand Chinese Yuan (CNY).
+#' @usage data(china_2017_149)
+#' @format A data frame with 156 rows and 164 columns.
+#' @source \href{https://data.stats.gov.cn/ifnormal.htm?u=/files/html/quickSearch/trcc/trcc01.html&h=740}{National Bureau of Statistics of China}
+"china_2017_149"
+
+
+#' @name china_2017_42
+#' @docType data
+#' @title Input-output table for China, 2017, 42 sectors
+#' @description The national input-output table of China for 2017, covering 42 sectors.
+#'     Values are denoted in 10 thousand Chinese Yuan (CNY).
+#' @usage data(china_2017_42)
+#' @format A data frame with 91 rows and 52 columns.
+#' @source \href{https://data.stats.gov.cn/ifnormal.htm?u=/files/html/quickSearch/trcc/trcc01.html&h=740}{National Bureau of Statistics of China}
+"china_2017_42"
+
+
+#' @name china_2018_153
+#' @docType data
+#' @title Input-output table for China, 2018, 153 sectors
+#' @description The national input-output table of China for 2018, covering 153 sectors.
+#'     Data are calculated at producers' prices in 2018.
+#'     Values are denoted in 10 thousand Chinese Yuan (CNY).
+#' @usage data(china_2018_153)
+#' @format A data frame with 160 rows and 168 columns.
+#' @source \href{https://data.stats.gov.cn/ifnormal.htm?u=/files/html/quickSearch/trcc/trcc01.html&h=740}{National Bureau of Statistics of China}
+"china_2018_153"
+
+
+#' @name china_2018_42
+#' @docType data
+#' @title Input-output table for China, 2018, 42 sectors
+#' @description The national input-output table of China for 2018, covering 42 sectors.
+#'     Values are denoted in 10 thousand Chinese Yuan (CNY).
+#' @usage data(china_2018_42)
+#' @format A data frame with 91 rows and 52 columns.
+#' @source \href{https://data.stats.gov.cn/ifnormal.htm?u=/files/html/quickSearch/trcc/trcc01.html&h=740}{National Bureau of Statistics of China}
+"china_2018_42"
+
+
+#' @name china_2020_153
+#' @docType data
+#' @title Input-output table for China, 2020, 153 sectors
+#' @description The national input-output table of China for 2020, covering 153 sectors.
+#'     Data are calculated at producers' prices in 2020.
+#'     Values are denoted in 10 thousand Chinese Yuan (CNY).
+#' @usage data(china_2020_153)
+#' @format A data frame with 160 rows and 168 columns.
+#' @source \href{https://data.stats.gov.cn/ifnormal.htm?u=/files/html/quickSearch/trcc/trcc01.html&h=740}{National Bureau of Statistics of China}
+"china_2020_153"
+
+
+#' @name china_2020_42
+#' @docType data
+#' @title Input-output table for China, 2020, 42 sectors
+#' @description The national input-output table of China for 2020, covering 42 sectors.
+#'     Values are denoted in 10 thousand Chinese Yuan (CNY).
+#' @usage data(china_2020_42)
+#' @format A data frame with 91 rows and 52 columns.
+#' @source \href{https://data.stats.gov.cn/ifnormal.htm?u=/files/html/quickSearch/trcc/trcc01.html&h=740}{National Bureau of Statistics of China}
+"china_2020_42"
