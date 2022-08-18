1decc17173a6a0e505b7783ecf85a566924b5175
Carol-seven <542046605@qq.com>
2022-08-18 12:22:01 +0800

    ionet-package.R

---
+++ R/ionet-package.R
@@ -0,0 +1,11 @@
+#' ionet
+#'
+#' Network Analysis for Input-Output Tables
+#'
+#' @docType package
+#' @author Shiying Xiao <shiying.xiao@outlook.com>
+#' @importFrom Rcpp evalCpp
+#' @useDynLib ionet, .registration = TRUE
+#' @name ionet
+
+NULL
