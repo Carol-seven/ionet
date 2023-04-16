2defd9e39d074bae4a6e8703acf6e58eff357887
Carol-seven <542046605@qq.com>
2023-04-16 23:36:22 +0800

    add references and note in data_WIOD()

--- R/data_WIOD.R
+++ R/data_WIOD.R
@@ -10,12 +10,34 @@
 #' @param version The specific release of WIOD, either "2013 Release" or "2016 Release".
 #' @param year The specific year of the data.
 #' @source \href{https://www.rug.nl/ggdc/valuechain/wiod/}{World Input-Output Database}
-#' @references Timmer, M. P., Dietzenbacher, E., Los, B., Stehrer, R. and de Vries, G. J. (2015).
-#' "An Illustrated User Guide to the World Input–Output Database: the Case of Global Automotive Production",
-#' \emph{Review of International Economics}. 23(3): 575--605.
-#' @note The size of the data may exceed the capacity of the internet connection,
+#' @references
+#' \itemize{
+#' \item Timmer, M. P., Los, B., Stehrer, R. and de Vries, G. J. (2016).
+#' "An Anatomy of the Global Trade Slowdown based on the WIOD 2016 Release",
+#' \emph{GGDC research memorandum number 162, University of Groningen}.
+#' \item Timmer, M. P., Dietzenbacher, E., Los, B., Stehrer, R. and de Vries, G. J. (2015).
+#' "An Illustrated User Guide to the World Input–Output Database: the Case of Global Automotive Production".
+#' \emph{Review of International Economics}, 23(3): 575--605.
+#' \item Dietzenbacher, E., Los, B., Stehrer R., Timmer, M. P. and de Vries, G. J. (2013).
+#' "The Construction of World Input-Output Tables in the WIOD Project".
+#' \emph{Economic Systems Research}, 25(1): 71--98.
+#' }
+#' @note
+#' \itemize{
+#' \item The size of the data may exceed the capacity of the internet connection,
 #' potentially limiting the download speed.
-#' @return NULL
+#' \item World input-output tables of the WIOD 2013 Release for the period from 1995 to 2011,
+#' covering 41 economies, each with 35 sectors.
+#' The format of the table is a data frame with 1443 rows and 1646 columns.
+#' Data are calculated in current prices.
+#' Values are denoted in millions of US dollars (USD).
+#' \item World input-output tables of the WIOD 2016 Release for the period from 2000 to 2014,
+#' covering 44 economies, each with 56 sectors.
+#' The format of the table is a data frame with 2472 rows and 2690 columns.
+#' Data are calculated in current prices.
+#' Values are denoted in millions of US dollars (USD).
+#' }
+#' @return The world input-output table of the WIOD specific version for the specific year.
 #' @export
 
 data_WIOD <- function(version = c("2013 Release", "2016 Release"),
